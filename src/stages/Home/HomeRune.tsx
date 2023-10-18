d46347c7e614bc76655dc1571a6ddcae984763b1
Dominik Supinski <dominik.supinski@getsavvy.com>
2023-10-18 10:41:28 +0100

    HomeRune magic numbers removed

--- src/stages/Home/HomeRune.tsx
+++ src/stages/Home/HomeRune.tsx
@@ -4,12 +4,10 @@ export const HomeRune = () => {
       className={"main-rune desktop z-10"}
       xmlns="http://www.w3.org/2000/svg"
       width="1600"
-      height="720.001"
-      viewBox="0 0 1600 720.001"
+      height="720"
+      viewBox="0 0 1600 720"
     >
       <path
-        id="Prostokąt_7_kopia_3"
-        data-name="Prostokąt 7 kopia 3"
         d="M1760,835H160V115H1760V835ZM825.89,292.31h0l-37.148,40.955L932.865,463.68V691.406h55.221V463.673L1131.9,333.2l-36.842-40.89L988.087,389.105V259.3H932.865V389.105L825.89,292.31Z"
         transform="translate(-160 -115)"
       ></path>
